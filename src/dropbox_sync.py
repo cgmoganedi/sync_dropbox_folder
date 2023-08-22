54f75f9fe233758f4c43dd23c168c93e4c07659a
Chuba <cgmoganedi@gmail.com>
2023-08-22 06:42:37 +0200

    nicer input prompts

--- src/dropbox_sync.py
+++ src/dropbox_sync.py
@@ -10,20 +10,24 @@ class DropboxSync:
         self.access_token = access_token
         self.dbx = dropbox.Dropbox(access_token)
 
-    def upload_file(self, local_path, remote_path):
+    def upload_file(self, local_path, remote_path)-> str:
         """Upload a local file to Dropbox."""
         try:
             with open(local_path, 'rb') as f:
                 self.dbx.files_upload(f.read(), remote_path)
+            return 'Success'
         except Exception as e:
             print(f"Error uploading file: {e}")
+            return 'Failed'
 
-    def download_file(self, remote_path, local_path):
+    def download_file(self, remote_path, local_path)-> str:
         """Download a file from Dropbox."""
         try:
             metadata, response = self.dbx.files_download(remote_path)
             with open(local_path, 'wb') as f:
                 f.write(response.content)
+            return 'Success'
         except Exception as e:
             print(f"Exception while downloading file: {e}")
+            return 'Failed'
 
--- src/main.py
+++ src/main.py
@@ -1,28 +1,30 @@
 import os
 from dropbox_sync import DropboxSync
 
-
+WELCOME = '\n📦 Welcome to Dropbox Scoped App file downloader/uploader! 🚀\n'
 def main():
-    access_token = os.getenv("DROPBOX_ACCESS_TOKEN")
+    access_token = os.getenv('DROPBOX_ACCESS_TOKEN')
     if not access_token:
-        raise ValueError("Dropbox access token not provided in .env file")
+        raise ValueError('Dropbox access token not provided in .env file')
 
     dropbox_sync = DropboxSync(access_token)
+    print(WELCOME)
 
-    upload = input('\nWould you like to upload a file? (yes/no) :')
+    upload = input('\nWould you like to upload a file 📄 ? (yes/no) : ')
     if upload == 'yes':
-        file_name = input(
-            '\nWhat is the name (with extention) of the file in the current working director? :')
+        file_name = '/' + input(
+            '\nWhat is the name (with extention) of the file in the current working director? : ./')
         completion = dropbox_sync.upload_file(file_name, file_name)
         print(completion)
 
-    download = input('\nWould you like to download a file? (yes/no) :')
+    download = input('\nWould you now like to download a file 📄 ? (yes/no) : ')
     if download == 'yes':
-        file_name = input(
-            '\nWhat is the name (with extention) of the remote file? :')
+        file_name = '/' + input(
+            '\nWhat is the path (with extention) of the remote file in the scoped app? : /')
         completion = dropbox_sync.download_file(file_name, file_name)
         print(completion)
 
+    print('... Bye! 👋\n')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
