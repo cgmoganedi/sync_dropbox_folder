3790315e36cc9309f9ce06f3ddb2476ba7dfd993
Chuba <cgmoganedi@gmail.com>
2023-08-22 03:59:24 +0200

    lets go for syncing entire dir

--- src/dropbox_sync.py
+++ src/dropbox_sync.py
@@ -11,7 +11,7 @@ class DropboxSync:
         self.dbx = dropbox.Dropbox(access_token)
 
     def upload_file(self, local_path, remote_path):
-        """Upload a file to Dropbox."""
+        """Upload a local file to Dropbox."""
         try:
             with open(local_path, 'rb') as f:
                 self.dbx.files_upload(f.read(), remote_path)
@@ -26,3 +26,24 @@ class DropboxSync:
                 f.write(response.content)
         except dropbox.exceptions.ApiError as e:
             print(f"Error downloading file: {e}")
+
+    def sync_directories(self, directory_one, directory_two, count=0):
+        """Sync all contents of a local directory with Dropbox."""
+        if count == 2:
+            return 'Success!'
+
+        try:
+            for root, _, files in os.walk(directory_one):
+                for file in files:
+                    local_path = os.path.join(root, file)
+                    relative_path = os.path.relpath(local_path, directory_one)
+                    remote_path = os.path.join(directory_two, relative_path)
+                    if count == 0:
+                        self.upload_file(local_path, remote_path)
+                    elif count == 1:
+                        self.download_file(local_path, remote_path)
+
+            return self.sync_directory(self, directory_two, directory_one, count+1)
+        except Exception as e:
+            print(
+                f'Problem syncing {directory_one} with {directory_two} on count={count}: ', str(e))
--- src/main.py
+++ src/main.py
@@ -4,16 +4,20 @@ from dropbox_sync import DropboxSync
 
 def main():
     access_token = os.getenv("DROPBOX_ACCESS_TOKEN")
+    local_directory_path = os.getenv("LOCAL_DIRECTORY")
+    remote_directory_path = os.getenv("REMOTE_DIRECTORY")
+
     if not access_token:
         raise ValueError("Dropbox access token not provided in .env file")
 
+    elif not local_directory_path or not remote_directory_path:
+        raise ValueError(
+            "Directory paths have been not provided well in .env file")
+
     dropbox_sync = DropboxSync(access_token)
-
-    local_file_path = "local_folder/file.txt"
-    remote_file_path = "/remote_folder/file.txt"
-
-    dropbox_sync.upload_file(local_file_path, remote_file_path)
-    dropbox_sync.download_file(remote_file_path, "downloaded_file.txt")
+    success = dropbox_sync.sync_directories(
+        local_directory_path, remote_directory_path)
+    print(success)
 
 
 if __name__ == "__main__":
