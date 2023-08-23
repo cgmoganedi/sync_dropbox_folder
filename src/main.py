0d71abcfbdaacab173d937342cf64ac38a7cebd3
Chuba <cgmoganedi@gmail.com>
2023-08-23 07:59:51 +0200

    put to lower case load env

--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
 import os
+from dotenv import load_dotenv
 from dropbox_sync import DropboxSync
 
 WELCOME = '\n📦 Welcome to Dropbox Scoped App file downloader/uploader! 🚀\n'
+
+# Load environment variables from .env file
+load_dotenv()
+
+
 def main():
     access_token = os.getenv('DROPBOX_ACCESS_TOKEN')
     if not access_token:
@@ -10,21 +16,26 @@ def main():
     dropbox_sync = DropboxSync(access_token)
     print(WELCOME)
 
-    upload = input('\nWould you like to upload a file 📄 ? (yes/no) : ')
-    if upload == 'yes':
-        file_name = '/' + input(
-            '\nWhat is the name (with extention) of the file in the current working director? : ./')
-        completion = dropbox_sync.upload_file(file_name, file_name)
+    upload = input('\nWould you like to upload a file 📄 ? (yes/no) : ').lower()
+
+    if upload == 'yes' or upload == 'y':
+        file_relative_path = input(
+            '\nWhat is the relative path (with extention) of the local file? : ./')
+
+        completion = dropbox_sync.upload_file(file_relative_path)
         print(completion)
 
-    download = input('\nWould you now like to download a file 📄 ? (yes/no) : ')
-    if download == 'yes':
-        file_name = '/' + input(
-            '\nWhat is the path (with extention) of the remote file in the scoped app? : /')
-        completion = dropbox_sync.download_file(file_name, file_name)
+    download = input(
+        '\nWould you now like to download a file 📄 ? (yes/no) : ').lower()
+    if download == 'yes' or download == 'y':
+        file_relative_path = '/' + input(
+            '\nWhat is the relative path (with extention) of the remote file in the scoped app? : /')
+
+        completion = dropbox_sync.download_file(file_relative_path)
         print(completion)
 
     print('... Bye! 👋\n')
+
 
 if __name__ == '__main__':
     main()
